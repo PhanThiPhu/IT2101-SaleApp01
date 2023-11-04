abea4f6cd92cb175bca08170877a96d04f3a3ada
2151050334phu@ou.edu <2151050334phu@ou.edu.vn>
2023-11-04 18:18:34 +0700

    add mysql

--- saleappv1/app/dao.py
+++ saleappv1/app/dao.py
@@ -1,45 +1,13 @@
+from app.models import Category, Product
+
+
 def load_categories():
-    return [{
-        'id': 1,
-        'name': 'Mobile'
-    }, {
-        'id': 2,
-        'name': 'Tablet'
-    }]
+    return Category.query.all()
 
 
 def load_products(kw=None):
-    products = [{
-        'id': 1,
-        'name': 'iPhone 15',
-        'price': 20000000,
-        'image': 'https://cdn.tgdd.vn/Products/Images/42/305660/iphone-15-pro-max-tu-nhien-1.jpg'
-    }, {
-        'id': 2,
-        'name': 'Galaxy S23',
-        'price': 20000000,
-        'image': 'https://cdn.tgdd.vn/Products/Images/42/305660/iphone-15-pro-max-tu-nhien-1.jpg'
-    }, {
-        'id': 3,
-        'name': 'Samsung A22',
-        'price': 20000000,
-        'image': 'https://cdn.tgdd.vn/Products/Images/42/305660/iphone-15-pro-max-tu-nhien-1.jpg'
-    }, {
-        'id': 4,
-        'name': 'iPhone 13',
-        'price': 20000000,
-        'image': 'https://cdn.tgdd.vn/Products/Images/42/305660/iphone-15-pro-max-tu-nhien-1.jpg'
-    }, {
-        'id': 5,
-        'name': 'iPhone 14',
-        'price': 20000000,
-        'image': 'https://cdn.tgdd.vn/Products/Images/42/305660/iphone-15-pro-max-tu-nhien-1.jpg'
-    }, {
-        'id': 6,
-        'name': 'iPhone 15 Pro',
-        'price': 20000000,
-        'image': 'https://cdn.tgdd.vn/Products/Images/42/305660/iphone-15-pro-max-tu-nhien-1.jpg'
-    }]
+    products = Product.query
+
     if kw:
-        products = [p for p in products if p['name'].find(kw) >= 0 ]
-    return products
+        products = products.filter(Product.name.contains(kw))
+    return products.all()
---
+++ saleappv1/app/models.py
@@ -0,0 +1,46 @@
+from sqlalchemy import Column, Integer, String, Float, ForeignKey
+from sqlalchemy.orm import relationship
+from app import db, app
+
+
+class Category(db.Model):
+    __tablename__ = 'category'
+
+    id = Column(Integer, primary_key=True, autoincrement=True)
+    name = Column(String(50), nullable=False, unique=True)
+    products = relationship('Product', backref='category', lazy=True)
+
+
+class Product(db.Model):
+    id = Column(Integer, primary_key=True, autoincrement=True)
+    name = Column(String(50), nullable=False, unique=True)
+    price = Column(Float, default=0)
+    image = Column(String(100))
+    category_id = Column(Integer, ForeignKey(Category.id), nullable=False)
+
+
+if __name__ == '__main__':
+    with app.app_context():
+        # c1 = Category(name='Mobile')
+        # c2 = Category(name='Tablet')
+        #
+        # db.session.add(c1)
+        # db.session.add(c2)
+        # db.session.commit()
+
+        p1 = Product(name='iPhone 15', price=20000000, category_id=1,
+                     image='https://cdn.tgdd.vn/Products/Images/42/305660/iphone-15-pro-max-tu-nhien-1.jpg')
+        p2 = Product(name='iPad 15', price=45000000, category_id=1,
+                     image='https://cdn.tgdd.vn/Products/Images/42/305660/iphone-15-pro-max-tu-nhien-1.jpg')
+        p3 = Product(name='Galaxy S23', price=27000000, category_id=1,
+                     image='https://cdn.tgdd.vn/Products/Images/42/305660/iphone-15-pro-max-tu-nhien-1.jpg')
+        p4 = Product(name='iPad Pro', price=33000000, category_id=2,
+                     image='https://cdn.tgdd.vn/Products/Images/42/305660/iphone-15-pro-max-tu-nhien-1.jpg')
+        p5 = Product(name='iPhone 14', price=54000000, category_id=1,
+                     image='https://cdn.tgdd.vn/Products/Images/42/305660/iphone-15-pro-max-tu-nhien-1.jpg')
+        p6 = Product(name='iPhone 13', price=20000000, category_id=2,
+                     image='https://cdn.tgdd.vn/Products/Images/42/305660/iphone-15-pro-max-tu-nhien-1.jpg')
+
+        db.session.add_all([p1, p2, p3, p4, p5, p6])
+        db.session.commit()
+        #db.create_all()
